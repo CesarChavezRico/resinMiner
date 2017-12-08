7310788fac862da00dfdb150396ec289a7881913
cesar <cesar.a.chavez@gmail.com>
2017-12-07 22:14:23 -0600

    cpu miner download and setup

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
+echo ..... start.sh: Setting up Mining Rig ...
+mkdir miner
+cd miner
+git clone https://github.com/novaspirit/wolf-m7m-cpuminer
+cd wolf-m7m-cpuminer
+./autogen.sh
+CFLAG="-O2 mfpu=neon-vfpv4" ./configure
+echo ----- miner setup end .. Makefile configuration missing!! -----
+
 echo ..... start.sh: Running main app ....
 python /usr/src/app/main.py
